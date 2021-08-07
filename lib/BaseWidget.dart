6655a689f96a5c27bc59077f18d6c9601c5aeb71
Dalton Hopkins <daltdalt1218@comcast.net>
2021-08-07 08:20:38 -0400

    started basewidget

--- lib/BaseWidget.dart
+++ lib/BaseWidget.dart
@@ -1,15 +1,30 @@
+import 'package:constrainer/BaseWidget/FileIO.dart';
+import 'package:constrainer/BaseWidget/MainState.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class BaseWidget extends StatefulWidget {
   @override
   BaseWidgetState createState() => BaseWidgetState();
 }
-class BaseWidgetState extends State<BaseWidget> {
 
+class BaseWidgetState extends State<BaseWidget> {
   @override
   Widget build(context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-    );
+    return FutureBuilder(
+        future: FileIO.readSave(),
+        builder: (context, AsyncSnapshot<Map<String, int>> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done &&
+              snapshot.hasData) {
+            return Provider(
+              create: (context) => MainState(snapshot.data!),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+              ),
+            );
+          } else {
+            return Center(child: CircularProgressIndicator());
+          }
+        });
   }
 }
