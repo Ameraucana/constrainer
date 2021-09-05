9d3375c7ea90956928203e096479ef910dab9ff3
Wyandotte <daltdalt1218@comcast.net>
2021-09-04 20:54:46 -0500

    switched to another json hosting service

--- lib/BaseWidget/FileIO.dart
+++ lib/BaseWidget/FileIO.dart
@@ -1,28 +1,65 @@
 import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:constrainer/BaseWidget/MainState.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart';
 
 class FileIO {
   static const String _endpoint =
-      r"https://api.jsonbin.io/v3/b/6133055e0825d31d4ed8a107";
+      r"https://json.psty.io/api_v1/stores/constrainer";
 
   static Future<String> get _apiKey async {
     return await rootBundle.loadString('assets/api_key');
   }
 
+  static Future<File> get _filePath async {
+    Directory docs = await getApplicationDocumentsDirectory();
+    return File(path.join(docs.path, "constrainer_data", "save.json"));
+  }
+
+  static Future<void> _ensureSaveExists() async {
+    File file = await _filePath;
+    bool fileExists = await file.exists();
+    if (!fileExists) {
+      await file.parent.create(recursive: true);
+      await file.writeAsString(jsonEncode({}));
+    }
+  }
+
   static Future<Map<String, dynamic>> readSave() async {
-    http.Response response = await http.get(Uri.parse("$_endpoint/latest"),
-        headers: {"X-Master-Key": await _apiKey, "X-Bin-Meta": "false"});
-    return Map<String, dynamic>.from(json.decode(response.body));
+    late Map<String, dynamic> data;
+    try {
+      http.Response response = await http
+          .get(Uri.parse("$_endpoint"), headers: {"Api-Key": await _apiKey});
+      data = json.decode(response.body)['data'];
+
+      // synchronize web with local here so 
+      // you don't have to induce it in-app
+      await _ensureSaveExists();
+      File file = await _filePath;
+      file.writeAsString(json.encode(data));
+    } catch (e) {
+      print("failed to retrieve data from server: $e");
+      
+      await _ensureSaveExists();
+      File file = await _filePath;
+      data = json.decode(await file.readAsString());
+    }
+
+    print(data);
+    return Map<String, dynamic>.from(data);
   }
 
   static Future<void> writeSave(MainState state) async {
+    await _ensureSaveExists();
+    File file = await _filePath;
+    await file.writeAsString(json.encode(state.content));
+
     await http.put(Uri.parse(_endpoint),
-        headers: {
-          "Content-Type": "application/json",
-          "X-Master-Key": await _apiKey
-        },
+        headers: {"Content-Type": "application/json", "Api-Key": await _apiKey},
         body: json.encode(state.content));
   }
 }
